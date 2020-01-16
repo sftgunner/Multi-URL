80c2fabeff5f657d566b36a37c9d68e873a83005
Sebastian Gunner <git@gunner237.dev>
2020-01-16 16:35:52 +0000

    Rewritten add.php; improved error reporting

--- add.php
+++ add.php
@@ -1,5 +1,5 @@
 <?php
-include "customfunctionlibrary.php";
+//include "customfunctionlibrary.php";
 if (!isset($_POST['g-recaptcha-response'])){
     header("Location: https://multiurl.sftg.io/?error=noCaptcha");
     die();
@@ -19,56 +19,21 @@ $data = array('secret' => getenv('RECAPTCHA_SECRET_MULTIURL'), 'response' => $ca
         );
         $context  = stream_context_create($options);
         $result = file_get_contents($url, false, $context);
-    if ($result === FALSE) { 
-        header("Location: https://multiurl.sftg.io/?error=captchaError");
-        die();
-    }
-    
-    $captcharesult = json_decode($result,1);
-    
-    //var_dump($captcharesult);
-    
-    if ($captcharesult["success"]){
-        
-        $dbserver = getenv('MYSQL_SERVER_MULTIURL');
-        $dbusername = getenv('MYSQL_USER_MULTIURL');
-        $dbpassword = getenv('MYSQL_PASSWORD_MULTIURL');
-        $dbname = getenv('MYSQL_DB_MULTIURL');
-        $dbport = getenv('MYSQL_PORT_MULTIURL');
-        $dbaccess = true;
-        
-        $urlsarr = $_POST['url'];
-        $urlsarr_sanatized = [];
-        
-        //Sanatise URLs
-        foreach ($urlsarr as $cururl){
-            $sanurl = filter_var($cururl, FILTER_SANITIZE_URL);
-            if (!filter_var($sanurl, FILTER_VALIDATE_URL) === false) {
-                //All good
-                array_push($urlsarr_sanatized,$sanurl);
-            } else {        
-                header("Location: https://multiurl.sftg.io/?error=invalidURL");
-                die();
-            }            
+        if ($result === FALSE) { 
+            header("Location: https://multiurl.sftg.io/?error=captchaError");
+            die();
         }
         
-        $urlnamearr = $_POST['name'];
-        $urlnamearr_sanatized = [];
+        $captcharesult = json_decode($result,1);
         
-        foreach ($urlnamearr as $curname){
-            array_push($urlnamearr_sanatized,filter_var($curname, FILTER_SANITIZE_STRING));
-        }
-        
-        //Combine into json
-        $urlsjson = json_encode(array_combine($urlsarr_sanatized,$urlnamearr_sanatized));
-        
-        $title = filter_var($_POST['pagetitle'], FILTER_SANITIZE_STRING);
-        
-        function generateshortlink(){
-            global $dbserver,$dbusername,$dbpassword,$dbname,$dbport;
-            //Generate random shortlink
-            $newshortlink = substr(md5(microtime()),rand(0,26),7);
-            //Check to see whether record with short name already exists
+        if ($captcharesult["success"]){
+            
+            $dbserver = getenv('MYSQL_SERVER_MULTIURL');
+            $dbusername = getenv('MYSQL_USER_MULTIURL');
+            $dbpassword = getenv('MYSQL_PASSWORD_MULTIURL');
+            $dbname = getenv('MYSQL_DB_MULTIURL');
+            $dbport = getenv('MYSQL_PORT_MULTIURL');
+            $dbaccess = true;
             
             // Create connection
             $mudbconn = new mysqli($dbserver, $dbusername, $dbpassword, $dbname, $dbport);
@@ -79,44 +44,93 @@ $data = array('secret' => getenv('RECAPTCHA_SECRET_MULTIURL'), 'response' => $ca
                 die("Database connection error");
             }
             
-            // prepare and bind
-            $stmt = $mudbconn->prepare("SELECT id FROM multiurl WHERE shortlink = ?");
-            $stmt->bind_param("s", $newshortlink);
+            $urlsarr = $_POST['url'];
+            $urlsarr_sanatized = [];
             
+            //Sanatise URLs
+            foreach ($urlsarr as $cururl){
+                $sanurl = filter_var($cururl, FILTER_SANITIZE_URL);
+                if (!filter_var($sanurl, FILTER_VALIDATE_URL) === false) {
+                    //All good
+                    array_push($urlsarr_sanatized,$sanurl);
+                } else {        
+                    header("Location: https://multiurl.sftg.io/?error=invalidURL");
+                    die();
+                }            
+            }
             
-            $getinfoResult = $stmt->execute();
-            $mysqlResult = $stmt->get_result();
-            $stmt->close();
+            $urlnamearr = $_POST['name'];
+            $urlnamearr_sanatized = [];
+            
+            foreach ($urlnamearr as $curname){
+                array_push($urlnamearr_sanatized,filter_var($curname, FILTER_SANITIZE_STRING));
+            }
+            
+            //Combine into JSON
+            $urlsjson = json_encode(array_combine($urlsarr_sanatized,$urlnamearr_sanatized));
+            
+            $title = filter_var($_POST['pagetitle'], FILTER_SANITIZE_STRING);
+            
+            function generateshortlink(){
+                //global $dbserver,$dbusername,$dbpassword,$dbname,$dbport;
+                global $mudbconn;
+                //Generate random shortlink
+                $newshortlink = substr(md5(microtime()),rand(0,26),7);
+                //Check to see whether record with short name already exists
+                
+                // prepare and bind
+                $stmt = $mudbconn->prepare("SELECT id FROM multiurl WHERE shortlink = ?");
+                $stmt->bind_param("s", mysqli_real_escape_string($newshortlink,$mudbconn));
+                
+                $getinfoResult = $stmt->execute();
+                $mysqlResult = $stmt->get_result();
+                $stmt->close();
+                
+                if ($mysqlResult->num_rows != 0){
+                    return false;
+                }
+                else{
+                    return $newshortlink;
+                }
+                
+            }
+            
+            $newshortlink = generateshortlink();
+            //Cycle through shorlinks until one that hasn't been found yet
+            while ($newshortlink == false) {
+                $newshortlink = generateshortlink();
+            }
+            
+            //$uploadresult = upload_to_db('multiurl','shortlink,count,urls,title',"'".$newshortlink."',0,'".$urlsjson."','".$title."'");
+            $table = 'multiurl';
+            $item = 'shortlink,count,urls,title';
+            $values = "'".$newshortlink."',0,'".$urlsjson."','".$title."'";
+
+            $uploaddataSQL = "INSERT INTO ".mysqli_real_escape_string($mudbcon,$table)." (".$item.") VALUES (".$values.")";
+            
+            // Fetch all
+            if (mysqli_query($mudbcon, $uploaddataSQL)) {
+                return "New record created successfully";
+            } else {
+                return "Error: " . $uploaddataSQL . "---" . mysqli_error($gundbcon);
+            }
+            
+            //Close DB Connection
             $mudbconn->close();
-            if ($mysqlResult->num_rows != 0){
-                return false;
+            
+            if ($uploadresult == "New record created successfully"){
+                header("Location: https://multiurl.sftg.io/".$newshortlink);
+                die();
             }
             else{
-                return $newshortlink;
+                header("Location: https://multiurl.sftg.io/?error=".$uploadresult);
+                die();
             }
-            
         }
-        
-        $newshortlink = generateshortlink();
-        
-        while ($newshortlink == false) {
-            $newshortlink = generateshortlink();
-        }
-        
-        $uploadresult = upload_to_db('multiurl','shortlink,count,urls,title',"'".$newshortlink."',0,'".$urlsjson."','".$title."'");
-        if ($uploadresult == "New record created successfully"){
-            header("Location: https://multiurl.sftg.io/".$newshortlink);
+        //Captcha failed
+        else{
+            header("Location: https://multiurl.sftg.io/?error=".$result);
             die();
         }
-        else{
-            header("Location: https://multiurl.sftg.io/?error=".$uploadresult);
-        }
-    }
-    else{
-        //header("Location: https://multiurl.sftg.io/?error=");
-
-        echo $result;
-        die();
-    }
-    
-    ?>
+        
+        ?>
